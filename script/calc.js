00df307dc5e3c34656a7bdb6fab7b0f1b72ad332
DOoOoOoOnut <intro2eun@gmail.com>
2024-01-10 09:54:12 +0900

    체중계 변수 등록

---
+++ script/calc.js
@@ -0,0 +1,8 @@
+// 1번 문제
+// DOM 변수 등록
+const userHeight = document.querySelector('#height');
+const userWeight = document.querySelector('#weight');
+const userGoal = document.querySelector('#goals');
+const resultBtn = document.querySelector('#result');
+// DOM 변수 데이터 정상 확인
+console.log(userHeight, userWeight, userGoal, resultBtn);
